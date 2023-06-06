e1196c3335db1a85c05c855850e1c234e2281609
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-06-06 15:14:02 +0200

    created a function

---
+++ E shop.app/src/Header/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+function Logo() {}
+
+ return ({
+    <div>
+    <div className="Logo"> E shop </div>
+     <div className="leftContainer">   
+      </div> 
+      </div>
+ }
+      )
+
+      export default Logo;
