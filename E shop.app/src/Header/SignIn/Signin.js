f8b1bf0b910b73b4b3cd13fffdf57b1b100d1666
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-06-11 19:28:17 +0200

    renamed a file

--- E shop.app/src/Header/SignIn/Signin.js
+++ E shop.app/src/Header/SignIn/Signin.js
@@ -2,7 +2,7 @@
 
 <div class="window"
 
-<h5> Please login </h5>
+<div class="header"> Please login </div>
 
 <div class="form-floating mb-3">
   <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
