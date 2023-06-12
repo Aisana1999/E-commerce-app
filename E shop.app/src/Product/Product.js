b0695a3bc2fba0223626a5955d0ba6bf7c9e6b27
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-06-12 15:46:14 +0200

    commented a usestate

--- E shop.app/src/Product/Product.js
+++ E shop.app/src/Product/Product.js
@@ -2,4 +2,8 @@ import { useState } from "react";
 import "./Product.css";
 function Product(props) {
   const tdata = props.content;
+  //const [star,setStar]=useState([]);
+  //const changeEvent=(event)=>{
+  //setStar(event.target.value);
+  //}
 }
