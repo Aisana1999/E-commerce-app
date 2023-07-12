1ecf7ce9de23a45ae131cd16897faa5b672820a1
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-07-12 16:23:45 +0200

    centered a div

--- E shop.app/src/components/Home/ProductCard/ProductCard.js
+++ E shop.app/src/components/Home/ProductCard/ProductCard.js
@@ -8,4 +8,12 @@ let stars = [];
 for (let i = 0; i < Number(values.item.rating.rate); i++) {
   stars[i] = 1;
 }
+
+return (
+  <div className="card">
+    <h5 className="card-title">{values.item.title}</h5>
+    <img src={values.item.image} alt="..."></img>
+    <hr />
+  </div>
+);
 export default ProductCard;
