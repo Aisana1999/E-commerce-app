78d63d9568846bc17873c6669b1fde0828e3a83d
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2023-07-20 15:57:30 +0200

    created useEffect

--- E shop.app/src/pages/Cart/Cart.js
+++ E shop.app/src/pages/Cart/Cart.js
@@ -3,6 +3,15 @@ import "./Cart.css";
 function Cart() {
   const [cartItems, setcartItem] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    fetch("product.json")
+      .then((res) => res.json())
+      .then((res) => {
+        updateTotalPrice(res);
+        setcartItem(res);
+      });
+  }, []);
 }
 
 export default Cart;
